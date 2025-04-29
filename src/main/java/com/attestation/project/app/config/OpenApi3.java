104c156a7224a303d5ce48a72417e900d1ac5b43
vanivice <ltd.vermut@mail.ru>
2025-04-29 15:03:10 +0800

    Update readme

---
+++ src/main/java/com/attestation/project/app/config/OpenApi3.java
@@ -0,0 +1,10 @@
+package com.attestation.project.app.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "PLACING-ORDERS", version = "1.0"))
+public class OpenApi3 {
+}
